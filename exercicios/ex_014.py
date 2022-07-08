2f8842e1abafb6def475281fc1c487e1fe510587
Gabriel Meirelles <gabriel.meirellessilva@hotmail.com>
2022-07-08 17:54:21 -0300

    Exercicios não mostrados no video

---
+++ exercicios/ex_014.py
@@ -0,0 +1,6 @@
+# faça um programa que converta temperatura de graus celcius para graus farenhait
+
+cel = float(input("Digite a temperatura em graus Celsius que converterei para Farenhait: "))
+far = (cel*(9/5))+32
+print("Dada a temperatura em Celsius a conversão para farenhait ficaria em média")
+print(f">>> {far:.1f}")
