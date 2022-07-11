6b8434af12cd8019ae1812aa2411495cb8967e2d
Gabriel Meirelles <gabriel.meirellessilva@hotmail.com>
2022-07-11 18:07:47 -0300

    Arrumado

--- exercicios/ex_017.py
+++ exercicios/ex_017.py
@@ -1,8 +1,7 @@
 # faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo-retângulo, calcule e mostre o compromento da hipotenusa
 
-from math import pow
-c_o = int(input("Qual o comprimento do cateto oposto?: "))
-c_ad = int(input("Qual o comprimento do cateto adjacente?: "))
-c_h = c_o+c_ad
-print(f"Bom segundo as medidas {c_o}² e {c_ad}² o cateto da hipotunusa é igual a {c_h}²")
-print(f"O cumprimento total do cateto da hipotenusa é igual a {pow(c_h, 2):.0f}")
+import math
+c_o =float(input("Qual o comprimento do cateto oposto?: "))
+c_ad = float(input("Qual o comprimento do cateto adjacente?: "))
+c_h = math.hypot(c_o, c_ad)
+print(f"Bom segundo as medidas {c_o} e {c_ad} o cateto da hipotunusa é igual a {c_h:.3f}")
