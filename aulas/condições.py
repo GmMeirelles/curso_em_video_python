692dbc546ddcb480b2332dde410379a4a21c44f0
Gabriel Meirelles <gabriel.meirellessilva@hotmail.com>
2022-07-25 19:55:06 -0300

    Aula dia 22/07 Att

--- aulas/condições.py
+++ aulas/condições.py
@@ -1,5 +1,7 @@
 # if = se 
-# elif = senão
+# else = senão
+# elif = senão se
+# usa-se dps de um if, podendo ter 1 if e varios elif sem else ou com else, mas lembre se nunk tera elif sem if
 
 print(' digite duas notas que irei fazer uma média e dizer se é boa ou não')
 num1 = float(input('>>>  '))
@@ -8,5 +10,9 @@ med = (num1+num2)/2
 if med <= 5:
     print('Com esta média você pode ficar de recuperação')
 
+elif med >5 and med <8:
+    print('Uma média boa mas da para melhorar')
+
+
 else:
-    print('Uma boa média, ja da para passar de ano :D')
+    print('Uma média ótima, ja da para passar de ano :D')
