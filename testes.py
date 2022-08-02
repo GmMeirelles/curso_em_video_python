f1943b9c096e6fca32516d82ab9ede6bbdc36107
Gabriel Meirelles <gabriel.meirellessilva@hotmail.com>
2022-08-02 19:12:28 -0300

    Area para testes

--- testes.py
+++ testes.py
@@ -1,2 +1,17 @@
-abc = input("Um nome: "), input('Outro nome: ')
-print(abc)
+print('Dê uma nota para o MeiMei de 0 a 10')
+n = int(input('>>>  '))
+
+if n > 7:
+    print('=-='*20)
+    print('Você ta certo o MeiMei é lindo goxtoso xeroso')
+    print('=-='*20)
+
+elif n > 5 or n < 8:
+    print('=-='*20)
+    print('Nhe mentira o MeiMei é gato papai')
+    print('=-='*20)
+
+else:
+    print('=-='*20)
+    print('MENTIRA O MEIMEI É UM GATO LEK')
+    print('=-='*20)
