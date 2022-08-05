10677385946614bb79d80449604f2f7f87a72d45
Gabriel Meirelles <gabriel.meirellessilva@hotmail.com>
2022-08-04 23:55:26 -0300

    Exercicios dia 02/08 (mod: 04/08)

--- exercicios/ex_054.py
+++ exercicios/ex_054.py
@@ -2,15 +2,15 @@
 
 from datetime import date
 
+total_menor = 0         # aqui criamos o total de pessoas de maior e menor
 total_maior = 0
-total_menor = 0
 
-for pessoa in range(1, 8):
-    nasc = int(input(f'Qual a data de nascimento da {pessoa} Pessoa?  '))
-    idade = date.today().year - nasc
+for pessoa in range(1, 8):          # aqui o for faz todo o codigo abaixo 7 vezes
+    nasc = int(input(f'Qual a data de nascimento da {pessoa} Pessoa?  '))       # pergunta a data de nascimento
+    idade = date.today().year - nasc                    # calcula a idade
     if idade >= 21:
-        total_maior += +1
+        total_maior += +1           # adiciona +1 ao total de pessoas maior de idade
 
     else:
-        total_menor += +1
+        total_menor += +1           # adiciona +1 ao total de pessoas menor de idade
 print(f'Ao todo temos {total_maior} pessoas de maior\nE {total_menor} pessoas de menor')
--- exercicios/ex_055.py
+++ exercicios/ex_055.py
@@ -1,18 +1,20 @@
 # leia o peso de 5 pessoas e diga qual pesa mais e qual pesa menos
 
-maior = 0
-menor = 0
+maior = 0       # aqui cria a variavel da coisa mais pesada até o momento 
+menor = 0       # e aqui da menor até o momento
 
-for pessoa in range(1, 6):
-    peso = float(input(f'Peso da {pessoa} Pessoa: '))
-    if pessoa == 1:
-        maior = pessoa
+for pessoa in range(1, 6):                  
+    peso = float(input(f'Peso da {pessoa} Pessoa: '))           # pergunta o peso de 6 pessoas e guarda na variavel peso
+    if pessoa == 1:                     # se for a primeira pessoa da lista, logo o maior e o menor peso vai ser dela
+        maior = pessoa                              
         menor = pessoa
-    else:
-        if peso > maior:
+    else:                       # se não
+        if peso > maior:           # se o peso da pessoa for maior q o antigo maior ele vai tomar o lugar do maior fazendo isso até chegar no final do laço de repetição
             maior = peso
-        if peso < menor:
+        if peso < menor:           # se o peso da pessoa for menor q o antigo menor ele vai tomar o lugar do menor fazendo isso até chegar no final do laço de repetição
             menor = peso
+
+# fazendo com que no fim do laço o maior peso fosse o maior colocado e o menor peso o menor colocado ai é só printar os pesos
 
 print(f'Maior Peso: {maior}')
 print(f'Menor peso: {menor}')
