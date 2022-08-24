9e8748a23d94ab7b0ec527d1bbed49a18f2b52f3
Gabriel Meirelles <gabriel.meirellessilva@hotmail.com>
2022-08-24 17:26:07 -0300

    Aulas dia 18/08

---
+++ aulas/interrompendo_repetições.py
@@ -0,0 +1,14 @@
+#break
+# usado para parar um laço de repetição while
+tal_coisa = 0
+while tal_coisa != 10:
+    if tal_coisa < 7:
+        tal_coisa += 1
+        print(f"Tal Coisa: {tal_coisa}")
+        break
+    if tal_coisa >= 8:
+        print(f'{tal_coisa} quase no fim')
+        tal_coisa += 1
+    if tal_coisa == 7:
+        tal_coisa += 1
+print(f'{tal_coisa} finalmente Tal coisa chegou a 10')
